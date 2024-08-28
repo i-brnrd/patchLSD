b5242ed45c80b6a94e38ce78e66c25a3be7abaf6
Isla Barnard <islarosemarybarnard@gmail.com>
2024-08-28 20:59:00 +0100

    patch simplified

--- Assets/Scripts/Patch.cs
+++ Assets/Scripts/Patch.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class Patch : MonoBehaviour
 {
-    private PatchManager patchManager;
-
     public GameObject BoxObj;
     private Box box;
     public GameObject fixationCross;
@@ -17,20 +15,22 @@ public class Patch : MonoBehaviour
     Color myBlue = new Color(0f / 255f, 0f / 255f, 142f / 255f);
     Color myRed = new Color(190f / 255f, 0f / 255f, 0f / 255f);
 
-    // Start is called before the first frame update
     private void Awake()
     {
        box  = BoxObj.GetComponent<Box>();
-       patchManager= GetComponent<PatchManager>();
     }
 
-    public void StartPatch(float[] patchArray, bool envB)
+    public IEnumerator StartPatch(float[] patchArray, bool envB)
     {
         eventCount = 0;
         rewards = patchArray;
-        Debug.Log(rewards.Length);
+        
         blueEnv = envB;
-        StartCoroutine(InterEvent());
+
+        while (eventCount < rewards.Length)
+        {
+            yield return StartCoroutine(InterEvent());
+        }
     }
    
     private IEnumerator InterEvent()
@@ -41,13 +41,8 @@ public class Patch : MonoBehaviour
             yield return null;
         }
 
-        NextEvent();
-    }
-
-    private void NextEvent()
-    {
         fixationCross.SetActive(false);
-        StartCoroutine(ShowBox());
+        yield return StartCoroutine(ShowBox());
     }
 
     private IEnumerator ShowBox()
@@ -66,27 +61,8 @@ public class Patch : MonoBehaviour
         reward = rewards[eventCount];
         box.SetBarHeight(reward);
         yield return new WaitForSeconds(EVENTDISPLAYTIME);
-        EndEvent();
-    }
-
-    private void EndEvent()
-    {
-        eventCount++;
         BoxObj.SetActive(false);
-        bool endPatch = (eventCount == rewards.Length);
-        Debug.Log(rewards.Length);
-
-        if (endPatch)
-        {
-            EndPatch();
-        }
-        else
-        {
-            StartCoroutine(InterEvent());
-        }
-
+        eventCount++;
     }
-
- 
 
 }
--- Assets/Scripts/PatchManager.cs
+++ Assets/Scripts/PatchManager.cs
@@ -6,10 +6,10 @@ public class PatchManager : MonoBehaviour
     // GameData Objects 
     public GameObject gameData;
     private GameData patchData;
+    private int trial;
 
     // Patch
     private Patch patch;
-    private int trial;
 
     // GameObjects 
     public LSD LSD;
@@ -29,14 +29,8 @@ public class PatchManager : MonoBehaviour
     public bool inChoicePhase = false; //choice phase 
     public bool? leave = null; //nullable bool. Null: not decided; leave = true, left; leave = false; stay 
 
-    private int[,] patchSets;
-    private bool[,] isUsed;
-
-    private int nSets = 18;
-    private int nPatches = 5; //per set 
-
-    // deal with this, is important 
-    private int availableSets = 5;
+   
+    private int counter = 0;
 
     private void Awake()
     {
@@ -48,28 +42,14 @@ public class PatchManager : MonoBehaviour
 
     public void StartTask()
     {
-        Debug.Log("hello I am a task");
         NextTrial();
-
-        // get 
     }
 
-    public void StartTraining()
-    {
-        Debug.Log("hello I am a training");
-        // whats a training patch going to look like 
-        // get a training patch & run it
-        // end of blue patch 
-        // get a defaul patch and run it
-        // end of red patch 
-        // repeat that like 3 times
-        // training completed - I think i need a Show Message thing 
-
-    }
-
+ 
     public void NextTrial()
     {
         trial = Random.Range(0, 89); // fix
+        counter = counter + 1;
         Debug.Log(trial.ToString());
         intertrialScreen.SetActive(true);
         leave = null;
@@ -81,13 +61,63 @@ public class PatchManager : MonoBehaviour
 
     private IEnumerator WaitForInput()
     {
+
         while (!Input.GetKeyDown(KeyCode.Space))
         {
             yield return null;
         }
         intertrialScreen.SetActive(false);
+
         NextPatch();
     }
+
+
+    public void NextPatch() // in Patch Manager I hope; and we get the trial number via random selection. 
+    {
+        if (leave == null)
+        {
+            envB = true;
+            rewards = patchData.rew2ld[trial];
+        }
+        else if (leave == true)
+        {
+            envB = false;
+            rewards = patchData.ldGo[trial];
+        }
+        else if (leave == false)
+        {
+            envB = true;
+            rewards = patchData.ldStay[trial];
+        }
+        Debug.Log(string.Join(", ", rewards));
+        Debug.Log(rewards.Length);
+        StartCoroutine(StartPatchCoroutine());
+        Debug.Log("Counter " + counter.ToString());
+    }
+
+    private IEnumerator StartPatchCoroutine()
+    {
+        Debug.Log("In StartPatchCoroutine");
+
+        Debug.Log(rewards);
+        Debug.Log(envB);
+        // Start the patch and wait until it is complete
+        yield return StartCoroutine(patch.StartPatch(rewards, envB));
+
+        Debug.Log("after StartCoroutine");
+
+        // After the patch is complete, decide what to do next
+        if (leave == null)
+        {
+            BeginChoicePhase();
+        }
+        else
+        {
+            NextTrial();
+        }
+    }
+
+
 
     public void BeginChoicePhase()
     {
@@ -115,57 +145,57 @@ public class PatchManager : MonoBehaviour
 
     }
 
-
-    public void NextPatch() // in Patch Manager I hope; and we get the trial number via random selection. 
-    {
-        if (leave == null)
-        {
-            envB = true;
-            rewards = patchData.rew2ld[trial];
-        }
-        else if (leave == true)
-        {
-            envB = false;
-            rewards = patchData.ldGo[trial];
-        }
-        else if (leave == false)
-        {
-            envB = true;
-            rewards = patchData.ldStay[trial];
-        }
-        Debug.Log(string.Join(", ", rewards));
-        Debug.Log(rewards.Length);
-        patch.StartPatch(rewards, envB);
-    }
-
-
-    //these will all be public 
-
-    public void InitPatchIdx()
-    {
-        // load in the actual patches elsewhere
-        // patchSets keeps track of the Row Number of each Patch in the correct index. 
-        for (int i = 0; i < nSets; i++)
-        {
-            for (int j = 0; j < nPatches; j++)
-            {
-                patchSets[i, j] = i * nSets + j;
-                isUsed[i, j] = false;
-            }
-        }
-
-
-    }
-
-
-    public void GetNextPatch()
-    {
-        // for now make it actually random
-
-    }
-
-   
-
-
 }
+
+
+////these will all be public 
+//private int[,] patchSets;
+//private bool[,] isUsed;
+
+//private int nSets = 18;
+//private int nPatches = 5; //per set 
+
+//// deal with this, is important 
+//private int availableSets = 5;
+
+//public void InitPatchIdx()
+//{
+//    // load in the actual patches elsewhere
+//    // patchSets keeps track of the Row Number of each Patch in the correct index. 
+//    for (int i = 0; i < nSets; i++)
+//    {
+//        for (int j = 0; j < nPatches; j++)
+//        {
+//            patchSets[i, j] = i * nSets + j;
+//            isUsed[i, j] = false;
+//        }
+//    }
+
+
+//}
+
+
+//public void GetNextPatch()
+//{
+//    // for now make it actually random
+
+//}
+
+//public void StartTraining()
+//{
+//    Debug.Log("hello I am a training");
+//    // whats a training patch going to look like 
+//    // get a training patch & run it
+//    // end of blue patch 
+//    // get a defaul patch and run it
+//    // end of red patch 
+//    // repeat that like 3 times
+//    // training completed - I think i need a Show Message thing 
+
+//}
+
+
+
+
+
 
